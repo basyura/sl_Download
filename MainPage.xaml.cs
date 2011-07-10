063f21174006176f645d8554cf095bee7def643a
basyura <basyura@gmail.com>
2011-07-10 15:12:22 +0900

    first commti

---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TryDownload
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var wc = new WebClient();
+            wc.OpenReadCompleted += new OpenReadCompletedEventHandler(wc_OpenReadCompleted);
+            wc.OpenReadAsync(new Uri("Webdings.ttf" , UriKind.Relative));
+
+        }
+
+        void wc_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
+        {
+            textBlock1.FontSource = new FontSource(e.Result);
+        }
+    }
+}
